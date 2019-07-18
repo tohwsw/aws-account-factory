3a2c6fd4c54e6b8d9747acd330182450f9711ddc
Toh <weitoh@8c8590a06e2a.ant.amazon.com>
2019-07-18 18:17:16 +0800

    initial

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# aws-account-factory
+# AWS Account Factory
+
+## Overview
+
+As an organization expands its use of AWS services, there is a need to create multiple AWS accounts to ensure separation of business processes or for security, compliance, and billing. Many of the customers we work with use separate AWS accounts for each business unit so they can meet the different needs of their organization. Creating multiple accounts has simplified operational issues and provided benefits like security and resource isolation, a smaller blast radius, and simplified billing. Often it takes a lot of time to create, bootstrap and configure baseline settings. 
+
+Customers want to manage account creation and bootstrapping in a scalable and efficient manner so that new accounts are created with a defined baseline and some governance guardrails are in place. Most importantly, customers want automation, to save time and resources.
+
+There are already 2 AWS solutions to quickly set up a secure, multi-account AWS environment based on AWS best practices. They are as follow:
+
+https://aws.amazon.com/solutions/aws-landing-zone/
+https://aws.amazon.com/controltower/
+
+However, the solutions above currently can only be applied to new accounts. The source code is also not available, so the solution cannot be augmented easily eg. taking away or replacing the SSO component.
+
+This repository contains the code to create an account vending machine (AVM) that can be applied into an existing AWS master account. Customers and partners of AWS can make use of this. If necessary augment it to include the guardrails required.
+
+An AVM will help you with the following:
+- Providing a user interface to provision new organization units and accounts
+- Create a new Amazon VPC in the new account
+- Lay down the account baseline including governance guardrails
+
+The tenets for the AVM are:
+- AVM follows the Middle Path to creating a landing zone. It seeks to be a efficient yet simple to maintain solution.
+- AVM will be able to be applied to existing as well as new accounts.
+
+## Solution Architecture
+
+The account builder is an AWS Service Catalog product that uses AWS Lambda and AWS Organizations APIs to create AWS accounts. On each invocation, the AWS Lambda function used in this sample solution does the following:
+
+1)Check if organization unit already exist, else create an organization unit under the root account in AWS Organizations
+2)Creates a new AWS account.
+3)Moves the newly created account from the organization root to the newly created organizational unit.
+4)Assumes the role OrganizationAccountAccessRole in the new account for the following:
+    - Deleting the default VPCs in all AWS Regions.
+    - Deploying a custom VPC using the provided parameters.
+    - Deploying the guardrails of AWS CloudTrail, AWS GuardDuty and AWS Config.
+
+This approach of bootstrapping accounts will reduce operational overhead and standardize account configurations across the provisioned AWS accounts. The following architecture outlines the process flow involved with account building :
+
+![img1]
+
+[img1]:https://github.com/tohwsw/aws-account-factory/blob/master/img/accountfactory.png
+
+## Step 1
+
+Upload baseline.json and index.py.zip(this contains the Lambda function) to your preferred S3 bucket.
+
+## Step 2
+
+Create a AWS Service Catalog with CloudFormation script createaccount.json.
+
+## Step 3
+
+Launch the product via AWS Service Catalog and log into the newly created AWS account by clicking the LoginURL.
+
+## CDK
+
+The baseline.json and createaccount.json are 
+
+
---
+++ index.py
@@ -0,0 +1,147 @@
+#Import modules
+import json, boto3, logging, os, time
+from botocore.vendored import requests
+
+#Define logging properties
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+
+def respond_cfn(event, status, data=None):
+    responseBody = {
+        'Status': status,
+        'Reason': 'See the details in CloudWatch Log Stream',
+        'PhysicalResourceId': event['ServiceToken'],
+        'StackId': event['StackId'],
+        'RequestId': event['RequestId'],
+        'LogicalResourceId': event['LogicalResourceId'],
+        'Data': data
+    }
+
+    print('Response = ' + json.dumps(responseBody))
+    print(event)
+    requests.put(event['ResponseURL'], data=json.dumps(responseBody))
+
+def assume_role(account_id, account_role):
+    sts_client = boto3.client('sts')
+    role_arn = 'arn:aws:iam::' + account_id + ':role/' + account_role
+
+    assumedRoleObject = sts_client.assume_role(
+        RoleArn=role_arn,
+        RoleSessionName="NewAccountRole"
+    )
+
+    return assumedRoleObject['Credentials']
+
+def create_account():
+    
+    #Initialisation of variables
+    client = boto3.client('organizations')
+    list_of_OU_ids = []
+    list_of_OU_names = []
+    newAccountId ='None'
+    ouId='None'
+    rootId=client.list_roots().get('Roots')[0].get('Id')
+
+    #Read and log the input values
+    acctName = os.environ['AccountName']
+    ouName = os.environ['OUName']
+    emailAddress = os.environ['Email']
+    log.info("Account name is: " + acctName)
+    log.info("Organizational Unit name is: " + ouName)
+    log.info("Email Address is: " + emailAddress)
+    
+    #Check if ou has been created
+    list_of_OUs_response = client.list_organizational_units_for_parent(ParentId=rootId)
+    
+    for i in list_of_OUs_response['OrganizationalUnits']:
+        list_of_OU_ids.append(i['Id'])
+        list_of_OU_names.append(i['Name'])
+        if(i['Name'] == ouName):
+            log.info("The provided Organization Unit already exist")
+            ouId=i['Id']
+    
+    if(ouName not in list_of_OU_names):
+        log.info("The provided Organization Unit Name doesnt exist. Creating an OU named: {}".format(ouName))
+        ou_creation_response = client.create_organizational_unit(ParentId=rootId,Name=ouName)
+        ouId = ou_creation_response['OrganizationalUnit']['Id']
+
+
+    #Create the account
+    acctResponse = client.create_account(
+        Email=emailAddress,
+        AccountName=acctName,
+        RoleName='OrganizationAccountAccessRole'
+    )
+
+    #Check Account Status
+    acctStatusID = acctResponse['CreateAccountStatus']['Id']
+    log.info(acctStatusID)
+
+    createStatus = client.describe_create_account_status(CreateAccountRequestId=acctStatusID)
+    newAccountId = createStatus.get('CreateAccountStatus').get('AccountId')  
+    while(newAccountId is None):
+        log.info("Account creation in progress")
+        time.sleep(5)
+        createStatus = client.describe_create_account_status(CreateAccountRequestId=acctStatusID)
+        newAccountId = createStatus.get('CreateAccountStatus').get('AccountId')
+    
+    log.info("New Account Id is " + newAccountId)
+
+    #Move Account to new OU
+    moveResponse = client.move_account(
+        AccountId=newAccountId,
+        SourceParentId=client.list_roots().get('Roots')[0].get('Id'),
+        DestinationParentId=ouId
+    )
+
+    return newAccountId
+
+def get_template(sourcebucket,baselinetemplate):
+
+    s3 = boto3.resource('s3')
+    content_object = s3.Object(sourcebucket,baselinetemplate)
+    obj = content_object.get()['Body'].read().decode('utf-8') 
+    return obj
+
+
+def create_baseline(template,credentials):
+    log.info(credentials['AccessKeyId'])
+    log.info(credentials['SecretAccessKey'])
+    client = boto3.client('cloudformation',
+                        region_name="ap-southeast-1",
+                        aws_access_key_id=credentials['AccessKeyId'],
+                          aws_secret_access_key=credentials['SecretAccessKey'],
+                          aws_session_token = credentials['SessionToken']
+                          )
+                          
+    print("Creating stack")
+    create_stack_response = client.create_stack(
+                    StackName='accountfactorybaseline',
+                    TemplateBody=template
+                )
+
+#Main Lambda function to be executed
+def main(event, context):
+    #Initialize the status of the function
+
+        try:
+            #Create a new Account in the OU Just Created
+            newAccountId = create_account()
+    
+            #Go into the new account
+            credentials=assume_role(newAccountId,'OrganizationAccountAccessRole')
+            
+            #Apply baselines
+            print("Waiting for account preparation")
+            time.sleep(60)
+            template = get_template('waynetohpublic','accountfactory/baseline.json')
+            create_baseline(template,credentials)
+
+            
+            respond_cfn(event,"SUCCESS",{"Message":"Account created successfully"})
+            return
+        except Exception:
+            respond_cfn(event,"FAILED",{"Message":"Account creation failed"})
+            log.exception("Lambda execution has failed!")
+            return
+
